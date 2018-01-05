6acc9a5abe1a60ad76da228e255fad0f7904fa8c
Paul Massardo <paul.massardo@gmail.com>
2018-01-05 11:30:35 -0500

    Add project files.

---
+++ DemoEventsCS/DemoEventsCS.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace DemoEventsCS
+{
+    partial class frmDemoEventsCS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.DemoTip = new System.Windows.Forms.ToolTip(this.components);
+            this.lbLabelDemonstrator = new System.Windows.Forms.Label();
+            this.tbTextBoxDemonstrator = new System.Windows.Forms.TextBox();
+            this.lbEvent = new System.Windows.Forms.Label();
+            this.lblEventOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(141, 30);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(82, 23);
+            this.btnEnter.TabIndex = 2;
+            this.btnEnter.Text = "&Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            this.btnEnter.MouseHover += new System.EventHandler(this.btnEnter_MouseHover);
+            // 
+            // btnReset
+            // 
+            this.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnReset.Location = new System.Drawing.Point(117, 266);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(199, 266);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lbLabelDemonstrator
+            // 
+            this.lbLabelDemonstrator.Location = new System.Drawing.Point(55, 7);
+            this.lbLabelDemonstrator.Name = "lbLabelDemonstrator";
+            this.lbLabelDemonstrator.Size = new System.Drawing.Size(80, 20);
+            this.lbLabelDemonstrator.TabIndex = 0;
+            this.lbLabelDemonstrator.Text = "&Demonstrator";
+            this.lbLabelDemonstrator.Click += new System.EventHandler(this.lbLabelDemonstrator_Click);
+            this.lbLabelDemonstrator.MouseHover += new System.EventHandler(this.lbLabelDemonstrator_MouseHover);
+            // 
+            // tbTextBoxDemonstrator
+            // 
+            this.tbTextBoxDemonstrator.Location = new System.Drawing.Point(141, 4);
+            this.tbTextBoxDemonstrator.Name = "tbTextBoxDemonstrator";
+            this.tbTextBoxDemonstrator.Size = new System.Drawing.Size(80, 20);
+            this.tbTextBoxDemonstrator.TabIndex = 1;
+            this.tbTextBoxDemonstrator.TextChanged += new System.EventHandler(this.tbTextBoxDemonstrator_TextChanged);
+            this.tbTextBoxDemonstrator.MouseHover += new System.EventHandler(this.tbTextBoxDemonstrator_MouseHover);
+            // 
+            // lbEvent
+            // 
+            this.lbEvent.Location = new System.Drawing.Point(4, 58);
+            this.lbEvent.Name = "lbEvent";
+            this.lbEvent.Size = new System.Drawing.Size(64, 20);
+            this.lbEvent.TabIndex = 3;
+            this.lbEvent.Text = "Event Data:";
+            // 
+            // lblEventOutput
+            // 
+            this.lblEventOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblEventOutput.Location = new System.Drawing.Point(5, 78);
+            this.lblEventOutput.Name = "lblEventOutput";
+            this.lblEventOutput.Size = new System.Drawing.Size(268, 185);
+            this.lblEventOutput.TabIndex = 4;
+            // 
+            // frmDemoEventsCS
+            // 
+            this.AcceptButton = this.btnEnter;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnReset;
+            this.ClientSize = new System.Drawing.Size(277, 289);
+            this.Controls.Add(this.lblEventOutput);
+            this.Controls.Add(this.lbEvent);
+            this.Controls.Add(this.tbTextBoxDemonstrator);
+            this.Controls.Add(this.lbLabelDemonstrator);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnEnter);
+            this.MaximizeBox = false;
+            this.Name = "frmDemoEventsCS";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "c# Events Demonstration";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmDemoEventsCS_FormClosing);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.frmDemoEventsCS_MouseDoubleClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ToolTip DemoTip;
+        private System.Windows.Forms.Label lbLabelDemonstrator;
+        private System.Windows.Forms.TextBox tbTextBoxDemonstrator;
+        private System.Windows.Forms.Label lbEvent;
+        private System.Windows.Forms.Label lblEventOutput;
+    }
+}
+
---
+++ DemoEventsCS/DemoEventsCS.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoEventsCS
+{
+    /// <summary>
+    /// Name:           DemoEventsCS
+    /// Author:         Alfred Masardo
+    /// Date:           01-Jan-2018
+    /// Description:    Demostrate some events and the data they produce.
+    /// </summary>
+    public partial class frmDemoEventsCS : Form
+    {
+        public frmDemoEventsCS()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// lbLabelDemonstrator_Click - Event that happens when the sender control is clicked, in this case a Label.
+        ///                - Displays event data.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lbLabelDemonstrator_Click(object sender, EventArgs e)
+        {
+
+            // sender is the control that has
+            Label control = (Label)sender;
+
+            // How the event was fired like... 
+            //      * Which mouse button was pressed. 
+            //      * How many clicks. 
+            //      * X coordinate of the mouse
+            //      * Y coordinate of the mouse
+            MouseEventArgs eventArguments = (MouseEventArgs)e;
+
+            // Display the event data
+            lblEventOutput.Text = "Event Handler: lbLabelDemonstrator_Click\n" +
+                                    "Event: Click\n" +
+                                    "Control: " + control.Name.ToString() + "\n" +
+                                    "Control Data: " + control.Text + "\n" +
+                                    "Mouse Button Clicked: " + eventArguments.Button + "\n" +
+                                    "# Of Clicks: " + eventArguments.Clicks.ToString() + "\n" +
+                                    "X Coordinates: " + eventArguments.X.ToString() + "\n" +
+                                    "Y Coordinates: " + eventArguments.Y.ToString();
+        }
+
+        /// <summary>
+        /// lbLabelDemonstrator_MouseHover - Event that happens when the mouse hovers over the sender control, in this case a Label.
+        ///                                 - Displays event data.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lbLabelDemonstrator_MouseHover(object sender, EventArgs e)
+        {
+
+            // sender is the control that is 
+            // sending the event
+            Label control = (Label)sender;
+
+            // Other Event arguments do not pass anything
+
+            // Display the event data
+            lblEventOutput.Text = "Event Handler: lbLabelDemonstrator_MouseHover\n" +
+                                    "Event: MouseHover\n" +
+                                    "Control: " + control.Name.ToString() + "\n" +
+                                    "Control Data: " + control.Text + "\n";
+
+
+        }
+
+        /// <summary>
+        /// tbTextBoxDemonstrator_TextChanged - Event that happens when the Text property of the sender control, in this case a TextBox, is changed.
+        ///                                     - Displays event data.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tbTextBoxDemonstrator_TextChanged(object sender, EventArgs e)
+        {
+
+            // sender is the control that is 
+            // sending the event
+            TextBox control = (TextBox)sender;
+
+            // Other Event arguments do not pass anything
+
+            // Display the event data
+            lblEventOutput.Text = "Event Handler: tbTextBoxDemonstrator_TextChanged\n" +
+                                    "Event: TextChanged\n" +
+                                    "Control: " + control.Name.ToString() + "\n" +
+                                    "Control Data: " + control.Text + "\n";
+
+        }
+
+        /// <summary>
+        /// tbTextBoxDemonstrator_MouseHover - Event that happens when the mouse hovers over the sender control, in this case a TextBox.
+        ///                                 - Displays event data.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tbTextBoxDemonstrator_MouseHover(object sender, EventArgs e)
+        {
+            // sender is the control that is 
+            // sending the event
+            TextBox control = (TextBox)sender;
+
+            // Other Event arguments do not pass anything
+
+            lblEventOutput.Text = "Event Handler: tbTextBoxDemonstrator_MouseHover\n" +
+                                    "Event: MouseHover\n" +
+                                    "Control: " + control.Name.ToString() + "\n" +
+                                    "Control Data: " + control.Text + "\n";
+        }
+
+        /// <summary>
+        /// btnEnter_Click - Event that happens when the sender control is clicked, in this case a Button.
+        ///                - Displays event data.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            // sender is the control that has
+            Button control = (Button)sender;
+
+            //// How the event was fired like... 
+            ////      * Which mouse button was pressed. 
+            ////      * How many clicks. 
+            ////      * X coordinate of the mouse
+            ////      * Y coordinate of the mouse
+
+            // If it was a mouse click
+            if (e is MouseEventArgs)
+            {
+
+                // get the Mouse Events
+                MouseEventArgs eventArguments = (MouseEventArgs)e;
+
+                // Display the event data
+                lblEventOutput.Text = "Event Handler: btnEnter_Click\n" +
+                                        "Event: MouseDoubleClick\n" +
+                                        "Control: " + control.Name.ToString() + "\n" +
+                                        "Control Data: " + control.Text + "\n" +
+                                        "Mouse Button Clicked: " + eventArguments.Button + "\n" +
+                                        "# Of Clicks: " + eventArguments.Clicks.ToString() + "\n" +
+                                        "X Coordinates: " + eventArguments.X.ToString() + "\n" +
+                                        "Y Coordinates: " + eventArguments.Y.ToString();
+            }
+            else // if the user tabbed to the button and click Enter
+            {
+
+                // since a mouse was not used there are no mouse events
+
+                // Display the event data
+                lblEventOutput.Text = "Event Handler: btnEnter_Click\n" +
+                                        "Event: Click\n" +
+                                        "Control: " + control.Name.ToString() + "\n" +
+                                        "Control Data: " + control.Text + "\n";
+            }
+        }
+
+        /// <summary>
+        /// btnEnter_MouseHover - Event that happens when the mouse hovers over the sender control, in this case a Button.
+        ///                     - Displays event data.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnEnter_MouseHover(object sender, EventArgs e)
+        {
+            // sender is the control that is 
+            // sending the event
+            Button control = (Button)sender;
+
+            // Other Event arguments do not pass anything
+
+            // Display the event data
+            lblEventOutput.Text = "Event Handler: btnEnter_MouseHover\n" +
+                                    "Event: MouseHover\n" +
+                                    "Control: " + control.Name.ToString() + "\n" +
+                                    "Control Data: " + control.Text + "\n";
+        }
+
+        /// <summary>
+        /// btnReset_Click - Event that happens when the sender control is clicked, in this case a Button.
+        ///                - Clears the screen.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+
+            // loop through the controls on the form
+            foreach (Control control in this.Controls)
+            {
+
+                if (control is TextBox) // if the control is a TextBox
+                {
+                    // empty the TextBox
+                    control.Text = string.Empty;
+                }
+                else if (control is Label) // if the control is a Label
+                {
+                    // output controls are set to have their BorderStyle
+                    // set to 3D, so if the label is set to have its 
+                    // BorderStyle set to Fixed3D 
+                    if (((Label)control).BorderStyle == BorderStyle.Fixed3D)
+                    {
+                        // empty the TextBox
+                        control.Text = string.Empty;
+                    }
+                }
+            }
+
+
+        }
+
+        /// <summary>
+        /// btnExit_Click - Event that happens when the sender control is clicked, in this case a Button.
+        ///               - Commences the process of closing the form.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Exit the form
+            this.Close();
+        }
+
+        /// <summary>
+        /// frmDemoEventsCS_FormClosing - Event that happens just prior to the form actually closing.
+        ///                             - Checks to see if the user actually wants to close the form.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmDemoEventsCS_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            // Get the results from the MessageBox
+            DialogResult resultOne = MessageBox.Show("Are you sure you wish you would like to close the form?", "Form Close", MessageBoxButtons.YesNo);
+
+            // If the user selected No
+            if (resultOne == DialogResult.No)
+            {
+                // Then Cancel the Closing of the form
+                e.Cancel = true;
+
+            }
+            else
+            {
+                // Get the results from the MessageBox
+                DialogResult resultTwo = MessageBox.Show("Really? You don't like me, FINE!", "Form Close", MessageBoxButtons.YesNo);
+
+                // If the user selected No
+                if (resultTwo == DialogResult.No)
+                {
+                    // Then Cancel the Closing of the form
+                    e.Cancel = true;
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// frmDemoEventsCS_MouseDoubleClick - Event that happens when the sender control (Form) is double clicked.
+        ///                - Displays event data.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmDemoEventsCS_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // sender is the control that has
+            Form control = (Form)sender;
+
+            //// How the event was fired like... 
+            ////      * Which mouse button was pressed. 
+            ////      * How many clicks. 
+            ////      * X coordinate of the mouse
+            ////      * Y coordinate of the mouse
+            MouseEventArgs eventArguments = e;
+
+            // Display the event data
+            lblEventOutput.Text = "Event Handler: frmDemoEventsCS_MouseDoubleClick\n" +
+                                    "Event: MouseDoubleClick\n" +
+                                    "Control: " + control.Name.ToString() + "\n" +
+                                    "Control Data: " + control.Text + "\n" +
+                                    "Mouse Button Clicked: " + eventArguments.Button + "\n" +
+                                    "# Of Clicks: " + eventArguments.Clicks.ToString() + "\n" +
+                                    "X Coordinates: " + eventArguments.X.ToString() + "\n" +
+                                    "Y Coordinates: " + eventArguments.Y.ToString();
+        }
+    }
+}
